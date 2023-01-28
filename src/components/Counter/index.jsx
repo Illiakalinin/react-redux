37f5f6609e6fb93f6cb7980373bc9a0472e69d5d
illiakalinin <illinoiz4305@gmail.com>
2023-01-28 15:41:46 +0200

    Add fixes from lesson rep

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
+
 import { connect } from 'react-redux';
 import { decrement, increment } from '../..';
 
 function Counter (props) {
   const { count, dispatch } = props;
+
+  //* dispatch викликається, коли треба сповістити стору
+  //* про необхідність змінити стан.
+  //* Як саме треба змінити стан вказує екшн в параметрах dispatch:
+  //* "dispatch надсилає екшн с стору"
+  const decrementCount = () => {
+    dispatch(decrement());
+  };
+
+  const incrementCount = () => {
+    dispatch(increment());
+  };
+
   return (
     <div>
       <div>{count}</div>
-      <button
-        onClick={() => {
-          dispatch(decrement());
-        }}
-      >
-        -
-      </button>
-      <button
-        onClick={() => {
-          dispatch(increment());
-        }}
-      >
-        +
-      </button>
+      <button onClick={decrementCount}>-</button>
+      <button onClick={incrementCount}>+</button>
     </div>
   );
 }
 
+//* функція - перший параметр connect -
+//* для прокидування стейту в пропси компонента
 function mapStateToProps (state) {
   return state;
 }
 
+//* connect створює HOC, який прокине в пропси компонента, що обгортається,
+//* dispatch - функції для сповіщення стори про необхідність змінити стан
 const HOC = connect(mapStateToProps);
 export default HOC(Counter);
