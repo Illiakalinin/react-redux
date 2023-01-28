cafa255b2be4161df2f9801f89e6ee74c817cd8e
illiakalinin <illinoiz4305@gmail.com>
2023-01-28 17:29:25 +0200

    Add payload

---
+++ src/store/slices/counterSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+//* createSlice - функція для створення:
+//* - стану (з його початковим станом),
+//* - редюсеру (функції для його зміни),
+//* - екшнів (об'єктів для сповіщення стори про те, що треба змінити стан)
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: { count: 0, step: 1 },
+  reducers: {
+    decrement (state, action) {
+      state.count = state.count - state.step;
+    },
+    increment (state, action) {
+      state.count = state.count + state.step;
+    },
+    setStep (state, action) {
+      state.step = action.payload;
+    },
+  },
+});
+
+const { reducer, actions } = counterSlice;
+
+export const { decrement, increment, setStep } = actions;
+
+export default reducer;
