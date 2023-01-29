196a0fce51d159f0e955d065a0deef8a6113553f
illiakalinin <illinoiz4305@gmail.com>
2023-01-29 17:49:12 +0200

    add async await ex

---
+++ src/sandbox.js
@@ -0,0 +1,18 @@
+// fetch('https://randomuser.me/api')
+//   .then(response => response.json())
+//   .then(data => console.log('data', data))
+//   .catch(err => console.log('err', err));
+
+async function fetchData () {
+  try {
+    const response = await fetch('https://randomuser.me/api');
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log('e :>>', e);
+  }
+}
+
+async function f () {} // => Promise
+
+console.log('fetchData()', fetchData());
